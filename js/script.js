490cd7d33370d4d14fa3372f70bd48edb790f029
Juho Savinainen <savinainen.juho@gmail.com>
2025-01-12 12:11:15 +0200

    updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-// Näyttää valitun osion ja piilottaa muut
 function showSection(sectionId) {
-    // Piilottaa kaikki sisältöalueet
     const sections = document.querySelectorAll('.content-section');
     sections.forEach(section => section.style.display = 'none');
 
-    // Näyttää vain valitun osion
     const activeSection = document.getElementById(sectionId);
     if (activeSection) {
         activeSection.style.display = 'block';
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => showSection('projects'));
